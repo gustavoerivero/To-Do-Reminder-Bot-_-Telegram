9aa00a8394fd14841dae7d4a7c47b724de46b7e5
Gustavo Rivero <gustavoerivero12@gmail.com>
2021-02-05 14:57:14 -0400

    Update remindmebot.py

--- remindmebot.py
+++ remindmebot.py
@@ -6,11 +6,11 @@ import pymongo
 from settings import config
 
 ## Bot
-bot = telebot.TeleBot("1624241159:AAGu2u6nBEiAr1xoNaS9bqxygLB7hQSLLUI")
+bot = telebot.TeleBot(config.telegram_key)
 dic_user = {}
 
 ## Setup Database
-client = pymongo.MongoClient("mongodb://admin:<1234>@mdb-shard-00-00.z34ey.mongodb.net:27017,mdb-shard-00-01.z34ey.mongodb.net:27017,mdb-shard-00-02.z34ey.mongodb.net:27017/<Telegram_RemindMeToDoBot>?ssl=true&replicaSet=atlas-c7xfze-shard-0&authSource=admin&retryWrites=true&w=majority")
+client = pymongo.MongoClient(config.mongodb_key)
 db_name = "Telegram_RemindMeToDoBot"
 collection_name = "users"
 db = client[db_name][collection_name]
