8eb9a9e3df3d4c30edc7ca952f40041438d92f4f
Gustavo Rivero <gustavoerivero12@gmail.com>
2021-02-05 14:51:52 -0400

    Create keys.py

---
+++ settings/keys.py
@@ -0,0 +1,2 @@
+mongodb_key = "mongodb://admin:<1234>@mdb-shard-00-00.z34ey.mongodb.net:27017,mdb-shard-00-01.z34ey.mongodb.net:27017,mdb-shard-00-02.z34ey.mongodb.net:27017/<Telegram_RemindMeToDoBot>?ssl=true&replicaSet=atlas-c7xfze-shard-0&authSource=admin&retryWrites=true&w=majority"
+telegram_key = "1624241159:AAGu2u6nBEiAr1xoNaS9bqxygLB7hQSLLUI"
